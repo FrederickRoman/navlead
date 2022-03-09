369dea94581ca55c141657c15ab0c8cfe795221e
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-03-08 17:04:24 -0800

    feat(index): Show Next.js-UnityWebGL integration proof of concept.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect } from "react";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "/static/script.js";
+    script.async = true;
+    document.body.appendChild(script);
+  });
   return (
     <div className={styles.container}>
       <Head>
@@ -13,12 +20,13 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <canvas id="unity-canvas" width="960" height="600"></canvas>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
@@ -59,14 +67,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ public/static/script.js
@@ -0,0 +1,95 @@
+var container = document.querySelector("#unity-container");
+var canvas = document.querySelector("#unity-canvas");
+var loadingBar = document.querySelector("#unity-loading-bar");
+var progressBarFull = document.querySelector("#unity-progress-bar-full");
+var fullscreenButton = document.querySelector("#unity-fullscreen-button");
+var warningBanner = document.querySelector("#unity-warning");
+
+// Shows a temporary message banner/ribbon for a few seconds, or
+// a permanent error message on top of the canvas if type=='error'.
+// If type=='warning', a yellow highlight color is used.
+// Modify or remove this function to customize the visually presented
+// way that non-critical warnings and error messages are presented to the
+// user.
+function unityShowBanner(msg, type) {
+  function updateBannerVisibility() {
+    warningBanner.style.display = warningBanner.children.length
+      ? "block"
+      : "none";
+  }
+  var div = document.createElement("div");
+  div.innerHTML = msg;
+  warningBanner.appendChild(div);
+  if (type == "error") div.style = "background: red; padding: 10px;";
+  else {
+    if (type == "warning") div.style = "background: yellow; padding: 10px;";
+    setTimeout(function () {
+      warningBanner.removeChild(div);
+      updateBannerVisibility();
+    }, 5000);
+  }
+  updateBannerVisibility();
+}
+
+var buildUrl = "/static/Build";
+var loaderUrl = buildUrl + "/ReleaseBuild.loader.js";
+var config = {
+  dataUrl: buildUrl + "/ReleaseBuild.data",
+  frameworkUrl: buildUrl + "/ReleaseBuild.framework.js",
+  codeUrl: buildUrl + "/ReleaseBuild.wasm",
+  streamingAssetsUrl: "StreamingAssets",
+  companyName: "DefaultCompany",
+  productName: "NavLead",
+  productVersion: "1.0",
+  showBanner: unityShowBanner,
+};
+
+// By default Unity keeps WebGL canvas render target size matched with
+// the DOM size of the canvas element (scaled by window.devicePixelRatio)
+// Set this to false if you want to decouple this synchronization from
+// happening inside the engine, and you would instead like to size up
+// the canvas DOM size and WebGL render target sizes yourself.
+// config.matchWebGLToCanvasSize = false;
+
+if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+  container.className = "unity-mobile";
+  // Avoid draining fillrate performance on mobile devices,
+  // and default/override low DPI mode on mobile browsers.
+  config.devicePixelRatio = 1;
+  unityShowBanner("WebGL builds are not supported on mobile devices.");
+} else {
+  canvas.style.width = "960px";
+  canvas.style.height = "600px";
+}
+// loadingBar.style.display = "block";
+
+var script = document.createElement("script");
+script.src = loaderUrl;
+var myGameInstance = null; //Added
+script.onload = () => {
+  createUnityInstance(canvas, config, (progress) => {
+    //progressBarFull.style.width = 100 * progress + "%";
+  })
+    .then((unityInstance) => {
+      myGameInstance = unityInstance; //Added
+      loadingBar.style.display = "none";
+      fullscreenButton.onclick = () => {
+        unityInstance.SetFullscreen(1);
+      };
+    })
+    .catch((message) => {
+      alert(message);
+    });
+};
+document.body.appendChild(script);
+
+//Added
+function sendRequestToUnity() {
+  console.log("Sending request to Unity");
+  myGameInstance.SendMessage("Player", "requestLocationFromUnity");
+}
+
+//Added
+function processMessageFromUnity(message) {
+  console.log(`Message received from Unity: ${message}`);
+}
