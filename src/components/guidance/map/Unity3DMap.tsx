8280e0d2d2e1af23937425aa39251c75cc61c4bf
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-03-09 01:47:20 -0800

    feat(Unity3DMap): Request and receive location on map

--- src/components/guidance/map/Unity3DMap.tsx
+++ src/components/guidance/map/Unity3DMap.tsx
@@ -1,5 +1,12 @@
 import { useEffect } from "react";
 
+declare global {
+  interface Window {
+    sendRequestToUnity: () => void;
+    processMessageFromUnity: () => void;
+  }
+}
+
 function Unity3DMap(): JSX.Element {
   useEffect(() => {
     const script = document.createElement("script");
@@ -8,22 +15,33 @@ function Unity3DMap(): JSX.Element {
     document.body.appendChild(script);
   }, []);
 
+  useEffect(() => {
+    window.addEventListener("unityResponse", (e) =>
+      console.log((e as CustomEvent).detail)
+    );
+  }, []);
+
+  const handleClick = () => window.sendRequestToUnity();
+
   return (
-    <div id="unity-container" className="unity-desktop">
-      <canvas id="unity-canvas" width="960" height="600"></canvas>
-      <div id="unity-loading-bar">
-        <div id="unity-logo" />
-        <div id="unity-progress-bar-empty">
-          <div id="unity-progress-bar-full" />
+    <>
+      <div id="unity-container" className="unity-desktop">
+        <canvas id="unity-canvas" width="960" height="600"></canvas>
+        <div id="unity-loading-bar">
+          <div id="unity-logo" />
+          <div id="unity-progress-bar-empty">
+            <div id="unity-progress-bar-full" />
+          </div>
+        </div>
+        <div id="unity-warning" />
+        <div id="unity-footer">
+          <div id="unity-webgl-logo" />
+          <div id="unity-fullscreen-button" />
+          <div id="unity-build-title">NavLead</div>
         </div>
       </div>
-      <div id="unity-warning" />
-      <div id="unity-footer">
-        <div id="unity-webgl-logo" />
-        <div id="unity-fullscreen-button" />
-        <div id="unity-build-title">NavLead</div>
-      </div>
-    </div>
+      <button onClick={handleClick}>Request Location</button>
+    </>
   );
 }
 
