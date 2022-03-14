73da6e1f8d83615d05be4ef43c24f796ff23fc92
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-03-14 16:24:08 -0700

    feat: Set up mui with nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,43 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+/**
+ * The setup for this page is based on the official mui-next example.
+ * It is used under MIT license (with same license notice as mui).
+ * @see https://github.com/mui/material-ui/blob/master/examples/nextjs/pages/_app.js
+ */
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider, StyledEngineProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+import createCache, { EmotionCache } from "@emotion/cache";
+import theme from "@/themes/theme";
 
-//import "/static/TemplateData/style.css"
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface Props {
+  Component: PropTypes.ReactComponentLike;
+  pageProps: object;
 }
 
-export default MyApp
+const cache: EmotionCache = createCache({
+  key: "css",
+  prepend: true,
+});
+cache.compat = true;
+
+function App(props: Props) {
+  const { Component, pageProps } = props;
+  return (
+    <CacheProvider value={cache}>
+      <Head>
+        <title>Navlead</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </CacheProvider>
+  );
+}
+
+export default App;
