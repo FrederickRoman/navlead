623e35a65a00e47b0f279bea94e98438c488c93e
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-03-14 16:28:18 -0700

    feat(themes): Add custom theme

---
+++ src/themes/theme.ts
@@ -0,0 +1,27 @@
+import { createTheme, Theme } from "@mui/material/styles";
+
+import { grey, blue, amber } from "@mui/material/colors";
+
+/**
+ * Triadic color palette
+ * primary:   #a158a3   purple
+ * secondary: #a158a3   amber
+ * tertiary:  #00825b   teal
+ */
+
+const theme: Theme = createTheme({
+  palette: {
+    mode: "light",
+    background: {
+      default: grey[100],
+    },
+    primary: {
+      main: "#a158a3",
+    },
+    secondary: {
+      main: "#ffbf00",
+    },
+  },
+});
+
+export default theme;
