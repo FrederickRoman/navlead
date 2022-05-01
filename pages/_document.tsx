6ce067fb52a44da9c745a65add1d7481e1c762c9
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-05-01 14:21:09 -0700

    feat(_document): Add Audiowide font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,9 +22,19 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta name="theme-color" content={theme.palette.primary.main} />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap"
           />
         </Head>
         <body>
