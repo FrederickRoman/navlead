7e8ab18ed2d90b2945fbb13b8f3a0309343cef20
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-05-01 18:42:53 -0700

    feat(NavBar): Add logoImg and font

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import {
   AppBar,
   Box,
@@ -8,15 +9,35 @@ import {
   Typography,
 } from "@mui/material";
 import HelpIcon from "@mui/icons-material/Help";
+import logoImg from "@/public/img/navlead_logo_light.png";
 
 function HomeLink(): JSX.Element {
   return (
     <Box>
       <Button>
-        <Grid container justifyContent="center" alignItems="center">
-          <Grid item>{/* Insert logo image here */}</Grid>
+        <Grid container justifyContent="center" alignItems="center" gap={0.1}>
           <Grid item>
-            <Typography variant="h6" sx={{ color: "background.default" }}>
+            <Grid container justifyContent="center" alignItems="center" >
+              <Grid item>
+                <Image
+                  priority
+                  width={22}
+                  height={22}
+                  alt="Person texting on a mobile chat app while driving"
+                  src={logoImg}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Typography
+              variant="h6"
+              sx={{
+                textTransform: "none",
+                fontFamily: "'Audiowide', cursive",
+                color: "#f3e5f5",
+              }}
+            >
               Navlead
             </Typography>
           </Grid>
