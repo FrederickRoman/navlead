63f8d11b5ef7257e07bd0955653f53338073041c
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-05-01 21:21:39 -0700

    refactor(guidance): Move useNavGuide to Unity3dScene

--- src/components/guidance/NavGuidance.tsx
+++ src/components/guidance/NavGuidance.tsx
@@ -1,34 +1,5 @@
-import { useEffect } from "react";
-import NavGuideService from "@/services/client/NavGuideService";
-import Traveler from "@/types/interfaces/Traveler";
-import Location from "@/types/interfaces/Location";
-import Unity3dScene from "./scene/Unity3dScene";
 import { Grid, Box, Typography } from "@mui/material";
-
-function useNavGuide(): void {
-  useEffect(() => {
-    const formatStringToLocation = (location: string): Location => {
-      const [x, y, z] = location.split(",").map(parseFloat);
-      return { x, y, z };
-    };
-    async function askNavGuide(event: Event): Promise<void> {
-      try {
-        const unityGameState = (event as CustomEvent).detail;
-        const gameStateObj = JSON.parse(unityGameState);
-        const { question, target, location: locationString } = gameStateObj;
-        const location: Location = formatStringToLocation(locationString);
-        const traveler: Traveler = { question, target, location };
-        const answer: string = await NavGuideService.answer(traveler);
-        window.sendAnswerToUnity(answer);
-        console.log(answer);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    window.addEventListener("askNavGuide", askNavGuide);
-    return () => window.removeEventListener("askNavGuide", askNavGuide);
-  }, []);
-}
+import Unity3dScene from "./scene/Unity3dScene";
 
 function NavInstructions(): JSX.Element {
   return (
@@ -52,7 +23,6 @@ function NavInstructions(): JSX.Element {
 }
 
 function NavGuidance(): JSX.Element {
-  useNavGuide();
   return (
     <Box
       component="section"
--- src/components/guidance/scene/Unity3dScene.tsx
+++ src/components/guidance/scene/Unity3dScene.tsx
@@ -1,39 +1,16 @@
-import { useEffect } from "react";
-import Location from "@/types/interfaces/Location";
+import { useState, useEffect } from "react";
+import type Location from "@/types/interfaces/Location";
+import type Traveler from "@/types/interfaces/Traveler";
+import NavGuideService from "@/services/client/NavGuideService";
 
 declare global {
   interface Window {
-    sendRequestToUnity: () => void;
     processMessageFromUnity: () => void;
     sendAnswerToUnity: (answer: string) => void;
   }
 }
 
-export class UnityMessager {
-  static async requestTravelerLocation(): Promise<Location> {
-    const unityMessageToLocation = (unityMessage: string): Location => {
-      const splitMsg = unityMessage.split(",");
-      const [x, y, z] = splitMsg.map(parseFloat);
-      return { x, y, z };
-    };
-    return new Promise((resolve, reject) => {
-      const TIMEOUT_DELAY = 3000;
-      const TIMEOUT_ERROR_MSG = "unity map location response took too long";
-      const rejectTimeout = () => reject(TIMEOUT_ERROR_MSG);
-      const timeoutId = setTimeout(rejectTimeout, TIMEOUT_DELAY);
-      const resolveLocation = (event: Event): void => {
-        clearTimeout(timeoutId);
-        const unityMessage = (event as CustomEvent).detail;
-        const location = unityMessageToLocation(unityMessage);
-        resolve(location);
-      };
-      window.addEventListener("unityResponse", resolveLocation, { once: true });
-      window.sendRequestToUnity();
-    });
-  }
-}
-
-function Unity3DMap(): JSX.Element {
+function useLoadUnityScript(): void {
   useEffect(() => {
     function appendUnityLoaderScript(): void {
       try {
@@ -47,6 +24,61 @@ function Unity3DMap(): JSX.Element {
     }
     appendUnityLoaderScript();
   }, []);
+}
+
+function useNavGuide(): void {
+  useEffect(() => {
+    const formatStringToLocation = (location: string): Location => {
+      const [x, y, z] = location.split(",").map(parseFloat);
+      return { x, y, z };
+    };
+    async function askNavGuide(event: Event): Promise<void> {
+      try {
+        const unityGameState = (event as CustomEvent).detail;
+        const gameStateObj = JSON.parse(unityGameState);
+        const { question, target, location: locationString } = gameStateObj;
+        const location: Location = formatStringToLocation(locationString);
+        const traveler: Traveler = { question, target, location };
+        const answer: string = await NavGuideService.answer(traveler);
+        window.sendAnswerToUnity(answer);
+        console.log(answer);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    window.addEventListener("askNavGuide", askNavGuide);
+    return () => window.removeEventListener("askNavGuide", askNavGuide);
+  }, []);
+}
+
+function useUnityCanvasResize(): [string, string] {
+  const DEFAULT_UNITY_CANVAS_WIDTH = "480px";
+  const DEFAULT_UNITY_CANVAS_HEIGHT = "300px";
+  const [width, setWidth] = useState<string>(DEFAULT_UNITY_CANVAS_WIDTH);
+  const [height, setHeight] = useState<string>(DEFAULT_UNITY_CANVAS_HEIGHT);
+
+  useEffect(() => {
+    function resizeUnityCanvas(): void {
+      if (/iPhone|iPad|iPod|Android/i.test(window.navigator.userAgent)) {
+        setWidth("95vw");
+        setHeight("calc(min(95vw/1.6, 90vh))");
+      } else {
+        setWidth("480px");
+        setHeight("300px");
+      }
+    }
+    resizeUnityCanvas();
+    window.addEventListener("resize", resizeUnityCanvas);
+    return () => window.removeEventListener("resize", resizeUnityCanvas);
+  }, []);
+
+  return [width, height];
+}
+
+function Unity3DMap(): JSX.Element {
+  useLoadUnityScript();
+  useNavGuide();
+  const [width, height] = useUnityCanvasResize();
 
   return (
     <>
@@ -55,12 +87,7 @@ function Unity3DMap(): JSX.Element {
         className="unity-desktop"
         style={{ display: "grid", placeContent: "center" }}
       >
-        <canvas
-          id="unity-canvas"
-          width="480" //960
-          height="300" //600
-          tabIndex={0}
-        ></canvas>
+        <canvas id="unity-canvas" width={width} height={height} tabIndex={0} />
         <div id="unity-loading-bar">
           <div id="unity-logo" />
           <div id="unity-progress-bar-empty">
