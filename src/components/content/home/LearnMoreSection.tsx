ff96b8f4930f7006f2a854499455cad47e554bbe
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-05-06 21:38:23 -0700

    feat(LearnMoreSection): Create section inviting to learn more and linking to about page

---
+++ src/components/content/home/LearnMoreSection.tsx
@@ -0,0 +1,25 @@
+import { Box, Grid, Button } from "@mui/material";
+import Link from "next/link";
+
+function LearnMoreSection(): JSX.Element {
+  return (
+    <Box
+      py={20}
+      sx={{
+        backgroundImage: "url(/img/cartographer_by_Sam_Feyaerts_under_CC.png)",
+      }}
+    >
+      <Grid container justifyContent="center" alignItems="center">
+        <Grid item>
+          <Link href="/about" passHref>
+            <Button component="a" variant="contained">
+              Learn More
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+export default LearnMoreSection;
