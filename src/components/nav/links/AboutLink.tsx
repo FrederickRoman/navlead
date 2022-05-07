af8cccd7c91964591dac1294a525eb78ad479b79
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-05-06 21:45:06 -0700

    feat(AboutLink): Link to /about page

--- src/components/nav/links/AboutLink.tsx
+++ src/components/nav/links/AboutLink.tsx
@@ -1,7 +1,22 @@
-import { Box } from "@mui/material";
+import Link from "next/link";
+import { Box, Typography } from "@mui/material";
 
 function AboutLink(): JSX.Element {
-  return <Box sx={{ color: "#f3e5f5" }}>About</Box>;
+  return (
+    <Link href="/about" passHref>
+      <Box component="a" sx={{ textDecoration: "none", color: "#f3e5f5" }}>
+        <Typography
+          variant="h6"
+          sx={{
+            textTransform: "none",
+            color: "#f3e5f5",
+          }}
+        >
+          About
+        </Typography>
+      </Box>
+    </Link>
+  );
 }
 
 export default AboutLink;
