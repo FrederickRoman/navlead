448160436c10a6bc7d1cc8d0f67fb06b001b145a
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-05-25 21:24:56 -0700

    feat(hrefs): Add more external links

--- src/constants/hrefs.ts
+++ src/constants/hrefs.ts
@@ -1,22 +1,34 @@
 const HREFS: Readonly<{ [key: string]: string }> = Object.freeze({
-    voxelDeskPage: "https://voxeldesk.vercel.app/",
-    voxelDeskGithub: "https://github.com/frederickroman",
-    blender: "https://www.blender.org/",
-    frederickRoman: "https://frederickroman.com",
-    homeroRoman: "https://homeroroman.com/",
-    frederickRomanLinkedIn: "https://www.linkedin.com/in/frederick-roman-9048673a",
-    homeroRomanLinkedIn: "https://www.linkedin.com/in/homeroromanroman",
-    frederickRomanGithub: "https://github.com/frederickroman",
-    homeroRomanGithub: "https://github.com/HomeroRR",
-    cvdnPage: "https://cvdn.dev/",
-    unityPage: "https://unity.com/",
-    unityDocsJs:
-      "https://docs.unity3d.com/Manual/webgl-interactingwithbrowserscripting.html",
-    djangoPage: "https://www.djangoproject.com/",
-    reactPage: "https://reactjs.org/",
-    webGlPage: "https://www.khronos.org/webgl/",
-    webAsmPage: "https://webassembly.org/",
-    seq2seq:"https://en.wikipedia.org/wiki/Seq2seq"
-  });
+  voxelDeskPage: "https://voxeldesk.vercel.app/",
+  voxelDeskGithub: "https://github.com/frederickroman",
+  blender: "https://www.blender.org/",
+  frederickRoman: "https://frederickroman.com",
+  homeroRoman: "https://homeroroman.com/",
+  frederickRomanLinkedIn:
+    "https://www.linkedin.com/in/frederick-roman-9048673a",
+  homeroRomanLinkedIn: "https://www.linkedin.com/in/homeroromanroman",
+  frederickRomanGithub: "https://github.com/frederickroman",
+  homeroRomanGithub: "https://github.com/HomeroRR",
+  cvdnPage: "https://cvdn.dev/",
+  unityPage: "https://unity.com/",
+  unityDocsJs:
+    "https://docs.unity3d.com/Manual/webgl-interactingwithbrowserscripting.html",
+  djangoPage: "https://www.djangoproject.com/",
+  reactPage: "https://reactjs.org/",
+  webGlPage: "https://www.khronos.org/webgl/",
+  webAsmPage: "https://webassembly.org/",
+  seq2seq: "https://en.wikipedia.org/wiki/Seq2seq",
+  nextjsPage: "https://nextjs.org/",
+  sunHouseOnCurbed:
+    "https://archive.curbed.com/maps/frank-lloyd-wright-best-buildings-map/norman-lykes-house-1967",
+  reactHooksPage: "https://reactjs.org/docs/hooks-intro.html",
+  pytorchPage: "https://pytorch.org/",
+  doorProblem:
+    "https://www.gamedeveloper.com/design/-quot-the-door-problem-quot-of-game-design",
+  restApiWiki:"https://en.wikipedia.org/wiki/Representational_state_transfer",
+  crossEntropy:"https://en.wikipedia.org/wiki/Cross_entropy",
+  teacherForcing:" https://en.wikipedia.org/wiki/Teacher_forcing",
+  blueScore:"https://en.wikipedia.org/wiki/BLEU"
+});
 
-  export default HREFS;
+export default HREFS;
