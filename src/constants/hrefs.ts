129ddbf85522ecbcd0d1453d1645c964b7cd86b2
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-05-25 17:11:07 -0700

    feat(hrefs): Add hrefs as constants

---
+++ src/constants/hrefs.ts
@@ -0,0 +1,22 @@
+const HREFS: Readonly<{ [key: string]: string }> = Object.freeze({
+    voxelDeskPage: "https://voxeldesk.vercel.app/",
+    voxelDeskGithub: "https://github.com/frederickroman",
+    blender: "https://www.blender.org/",
+    frederickRoman: "https://frederickroman.com",
+    homeroRoman: "https://homeroroman.com/",
+    frederickRomanLinkedIn: "https://www.linkedin.com/in/frederick-roman-9048673a",
+    homeroRomanLinkedIn: "https://www.linkedin.com/in/homeroromanroman",
+    frederickRomanGithub: "https://github.com/frederickroman",
+    homeroRomanGithub: "https://github.com/HomeroRR",
+    cvdnPage: "https://cvdn.dev/",
+    unityPage: "https://unity.com/",
+    unityDocsJs:
+      "https://docs.unity3d.com/Manual/webgl-interactingwithbrowserscripting.html",
+    djangoPage: "https://www.djangoproject.com/",
+    reactPage: "https://reactjs.org/",
+    webGlPage: "https://www.khronos.org/webgl/",
+    webAsmPage: "https://webassembly.org/",
+    seq2seq:"https://en.wikipedia.org/wiki/Seq2seq"
+  });
+
+  export default HREFS;
