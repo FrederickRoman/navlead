eebe25a32668a0cc25aca056a487dca8648a00f2
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-05-25 17:33:34 -0700

    feat(CodeSection): Show code on Paper with codeName

--- src/components/code/CodeSection.tsx
+++ src/components/code/CodeSection.tsx
@@ -1,25 +1,49 @@
 import dynamic from "next/dynamic";
-import { Grid, Box } from "@mui/material";
+import { Grid, Box, Paper, Stack } from "@mui/material";
 
 interface Props {
-  codeLang: "javascript" | "python" | "csharp";
+  codeName?: string;
+  codeLang: "javascript" | "python" | "csharp" | "tsx";
   codeText: string;
 }
 
 const SyntaxHighlightedCode = dynamic(
   () => import("./highlight/SyntaxHighlightedCode"),
-  { ssr: false }
+  { ssr: false } // Must be turned off to prevent inconsistencies in rendering
 );
 
 function CodeSection(props: Props): JSX.Element {
-  const { codeLang, codeText } = props;
+  const { codeName, codeLang, codeText } = props;
 
   return (
     <Grid container justifyContent="center" alignItems="center">
       <Grid item>
-        <Box width="90vw" height="auto">
-          <SyntaxHighlightedCode codeLang={codeLang} codeText={codeText} />
-        </Box>
+        <Stack>
+          {codeName && (
+            <Box
+              width="90vw"
+              border="1px solid black"
+              style={{
+                backgroundColor: "black",
+                color: "#0CFE6F",
+                textAlign: "center",
+                fontSize: 20,
+                overflow: "auto"
+              }}
+            >
+              {codeName}
+            </Box>
+          )}
+          <Box
+            component={Paper}
+            square
+            width="90vw"
+            height="auto"
+            style={{ background: "#1e1e1e" }}
+          >
+            <SyntaxHighlightedCode codeLang={codeLang} codeText={codeText} />
+          </Box>
+        </Stack>
       </Grid>
     </Grid>
   );
