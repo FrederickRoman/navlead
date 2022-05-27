b44d833ecfc4250ef1a6938831c4594c7f1839ef
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-05-27 12:13:39 -0700

    feat(ProxyService): Add api key as env variable

--- src/services/server/ProxyService.ts
+++ src/services/server/ProxyService.ts
@@ -6,8 +6,12 @@ const API_URL = "http://127.0.0.1:8000/navigation/";
 class NavGuide {
   static async awake(): Promise<void> {
     try {
-      const body = { question: "", target: "", location: "" };
-      await axios.post(API_URL, body);
+      await axios.post(API_URL, {
+        question: "",
+        target: "",
+        location: "",
+        API_KEY: process.env.ML_SERVER_API_KEY,
+      });
     } catch (error) {
       console.log(error);
     }
@@ -15,7 +19,7 @@ class NavGuide {
   static async answer(traveler: Traveler): Promise<string> {
     try {
       console.log("proxy");
-      const body = traveler;
+      const body = {...traveler, API_KEY: process.env.ML_SERVER_API_KEY};
       const res = await axios.post(API_URL, body);
       return res.data.answer;
     } catch (error) {
