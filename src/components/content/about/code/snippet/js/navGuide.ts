e50606191e42b645e6dbc8a44963d2820733d065
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-05-27 21:00:31 -0700

    feat(snippet): Add comments to navGuide

--- src/components/content/about/code/snippet/js/navGuide.ts
+++ src/components/content/about/code/snippet/js/navGuide.ts
@@ -1,25 +1,41 @@
 const navGuide = `import { useEffect } from "react";
 import NavGuideService from "services/NavGuideService";
 
+/* 
+ * Type of functions that communicate the simulation with the client browser
+ * through the window object
+ */
 declare global {
     interface Window {
-      processMessageFromUnity: () => void;
-      sendAnswerToUnity: (answer: string) => void;
+      processMessageFromUnity: () => void; // from simulatin to client
+      sendAnswerToUnity: (answer: string) => void; // from client to simulation
     }
 }
 
+/* 
+ * Type of the traveler's location in the simulation
+ */
 interface Location {
   x: number;
   y: number;
   z: number;
 }
 
+/* 
+ * Type of the traveler's state
+ */
 interface Traveler {
   question: string;
   target: string;
   location: Location;
 }
   
+/*
+ * When a question gets submitted, useNavGuide collects the traveler's full state
+ * (which includes the question), and then sends it to the NavGuideService. 
+ * After NavGuideService sends back an answer, 
+ * the useNavGuide resends the answer to the Unity simulation
+ */
 function useNavGuide(): void {
     useEffect(() => {
       const formatStringToLocation = (location: string): Location => {
@@ -28,12 +44,15 @@ function useNavGuide(): void {
       };
       async function askNavGuide(event: Event): Promise<void> {
         try {
+          /* Collect the traveler's full state */
           const unityGameState = (event as CustomEvent).detail;
           const gameStateObj = JSON.parse(unityGameState);
           const { question, target, location: locationString } = gameStateObj;
           const location: Location = formatStringToLocation(locationString);
           const traveler: Traveler = { question, target, location };
+          /* Ask NavGuideService to answer traveler's question */
           const answer: string = await NavGuideService.answer(traveler);
+          /* Send answer to simulation */
           window.sendAnswerToUnity(answer);
         } catch (error) {
           console.log(error);
